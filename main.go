1388725bbb41abea5e51388cb1cdedbef9a44300
LuizCampedelli <vapeprosper@gmail.com>
2024-09-04 12:14:53 -0300

    middle main.go code

--- main.go
+++ main.go
@@ -2,14 +2,26 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
-	"html/template"
+	"os"
 	"strconv"
 )
 
-type Calcultor struct {
-	Result string
-}
+func main() {
+	// checa se o número de argumentos é o esperado
+	if len(os.Args) < 4 {
+		fmt.Println("Usage: calculator <number1> <operator> <number2>")
+		return
+	}
 
+	// converte os argumentos para os tipos corretos
+	num1, err1 := strconv.ParseFloat(os.Args[1], 64)
+	operator:= os.Args[2]
+	num2, err2 := strconv.ParseFloat(os.Args[3], 64)
 
+	// checa se houve erro na conversão
+	if err1 != nil || err2 != nil {
+		fmt.Println("Invalid number")
+		return
+	}
+
+	
